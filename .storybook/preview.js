e93c0749ae03e42be67981983b33784790cc2cb8
Chacha Sikes <chachasikes@gmail.com>
2020-12-30 08:14:31 -0800

    add addon components: a11y, storybook-addon-designs and storyshots (which is related to tests), added mock data container & example of embedding a figma frame (from a test file).

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import { withConsole, setConsoleOptions } from "@storybook/addon-console";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  viewport: {
+    viewports: MINIMAL_VIEWPORTS,
+  },
+};
+
+setConsoleOptions({
+  panelExclude: [],
+});
--- stories/web-components/cagov-reopening/cagov-reopening.stories.js
+++ stories/web-components/cagov-reopening/cagov-reopening.stories.js
@@ -1,4 +1,10 @@
+import { withDesign } from "storybook-addon-designs";
+import { data } from './data/california.js';
 import { Button } from './cagov-reopening';
+
+let mockData = {
+  Default: data
+};
 
 export default {
   title: 'covid19/cagov-reopening/web-component',
@@ -6,9 +12,18 @@ export default {
     backgroundColor: { control: 'color' },
     onClick: { action: 'onClick' },
   },
+  // component: Button,
+  decorators: [withDesign]
 };
 
 const Template = (args) => Button(args);
+
+/**
+* Connect mock data
+*/
+Template.args = {
+    data: data,
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
@@ -16,19 +31,30 @@ Primary.args = {
   label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+/**
+* Connect Figma Frame
+*/
+Primary.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/hX1EkAcCQ86bzJKf6ZYy9y/Untitled?node-id=1%3A2",
+  },
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
